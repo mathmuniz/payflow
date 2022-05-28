c3b8279b10bdef328300a04e55bdfca11d810ef0
mathmuniz <mmuniz605@gmail.com>
2022-05-28 18:45:25 -0300

    increment page

--- lib/modules/insert_boleto/insert_boleto_page.dart
+++ lib/modules/insert_boleto/insert_boleto_page.dart
@@ -1,5 +1,9 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/input_text/input_text_widget.dart';
 
 class InsertBoletoPage extends StatelessWidget {
   const InsertBoletoPage({super.key});
@@ -7,8 +11,29 @@ class InsertBoletoPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: AppColors.background,
       appBar: AppBar(
-        title: Text('Insert Boleto Page'),
+          backgroundColor: AppColors.background,
+          elevation: 0,
+          leading: BackButton(
+            color: AppColors.input,
+          )),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 93,
+              vertical: 24,
+            ),
+            child: Text(
+              'Preencha os dados do boleto',
+              style: TextStyles.titleBoldHeading,
+              textAlign: TextAlign.center,
+            ),
+          ),
+          InputTextWidget()
+        ],
       ),
     );
   }
