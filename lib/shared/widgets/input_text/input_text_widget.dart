e795e9e6169ad2ee9c6c9e108523741419a9b058
mathmuniz <mmuniz605@gmail.com>
2022-05-29 20:10:15 -0300

    add

--- lib/shared/widgets/input_text/input_text_widget.dart
+++ lib/shared/widgets/input_text/input_text_widget.dart
@@ -2,37 +2,67 @@
 
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
 class InputTextWidget extends StatelessWidget {
-  const InputTextWidget({super.key});
+  final String label;
+  final IconData icon;
+  final String? initialValue;
+  final String? Function(String?)? validator;
+  final TextEditingController? controller;
+  final void Function(String value) onChanged;
+
+  const InputTextWidget({
+    super.key,
+    required this.label,
+    required this.icon,
+    this.initialValue,
+    this.validator,
+    this.controller,
+    required this.onChanged,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        TextFormField(
-            decoration: InputDecoration(
-          icon: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 18),
-                child: Icon(Icons.person),
-              ),
-              Container(
-                width: 1,
-                height: 48,
-                color: AppColors.stroke,
-              ),
-            ],
-          ),
-          border: InputBorder.none,
-        )),
-        Divider(
-          height: 1,
-          thickness: 1,
-          color: AppColors.stroke,
-        )
-      ],
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 16),
+      child: Column(
+        children: [
+          TextFormField(
+              controller: controller,
+              onChanged: onChanged,
+              initialValue: initialValue,
+              style: TextStyles.input,
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.zero,
+                labelText: label,
+                labelStyle: TextStyles.input,
+                icon: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 18),
+                      child: Icon(
+                        icon,
+                        color: AppColors.primary,
+                      ),
+                    ),
+                    Container(
+                      width: 1,
+                      height: 48,
+                      color: AppColors.stroke,
+                    ),
+                  ],
+                ),
+                border: InputBorder.none,
+              )),
+          Divider(
+            height: 1,
+            thickness: 1,
+            color: AppColors.stroke,
+          )
+        ],
+      ),
     );
   }
 }
