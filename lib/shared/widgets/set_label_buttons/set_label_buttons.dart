e1f5aace84c223b8459fc48e078de3be936a6af3
mathmuniz <mmuniz605@gmail.com>
2022-05-29 20:09:37 -0300

    update

--- lib/shared/widgets/set_label_buttons/set_label_buttons.dart
+++ lib/shared/widgets/set_label_buttons/set_label_buttons.dart
@@ -12,34 +12,51 @@ class SetLabelButtons extends StatelessWidget {
   final String secondaryLabel;
   final VoidCallback secondaryOnPressed;
   final bool enablePrimaryColor;
-  const SetLabelButtons({super.key,
+  final bool enableSecondaryColor;
+
+  const SetLabelButtons({
+    super.key,
     required this.primaryLabel,
     required this.primaryOnPressed,
     required this.secondaryLabel,
     required this.secondaryOnPressed,
-    this.enablePrimaryColor = false, 
-
+    this.enablePrimaryColor = false,
+    this.enableSecondaryColor = false,
   });
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: AppColors.shape,
-      height: 56,
-      child: Row(
+      color: AppColors.background,
+      height: 57,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
         children: [
-          Expanded(
-              child: LabelButton(
-            label: primaryLabel,
-            onPressed: primaryOnPressed,
-            style: enablePrimaryColor ? TextStyles.buttonPrimary : null,
-          )),
-          DividerVerticalWidget(),
-          Expanded(
-              child: LabelButton(
-            label: secondaryLabel,
-            onPressed: secondaryOnPressed,
-          )),
+          Divider(
+            thickness: 1,
+            height: 1,
+            color: AppColors.stroke,
+          ),
+          Container(
+            height: 56,
+            child: Row(
+              children: [
+                Expanded(
+                    child: LabelButton(
+                  label: primaryLabel,
+                  onPressed: primaryOnPressed,
+                  style: enablePrimaryColor ? TextStyles.buttonPrimary : null,
+                )),
+                DividerVerticalWidget(),
+                Expanded(
+                    child: LabelButton(
+                  label: secondaryLabel,
+                  onPressed: secondaryOnPressed,
+                  style: enableSecondaryColor ? TextStyles.buttonPrimary : null,
+                )),
+              ],
+            ),
+          ),
         ],
       ),
     );
