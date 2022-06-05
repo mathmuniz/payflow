7da9a7678c3c1ec44cdd3f9542ee517f2bea833f
mathmuniz <mmuniz605@gmail.com>
2022-06-05 17:22:26 -0300

    add

---
+++ lib/shared/widgets/boleto_list/boleto_list_widget.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/models/boleto_model.dart';
+import 'package:payflow/shared/widgets/boleto_list/boleto_list_controller.dart';
+import 'package:payflow/shared/widgets/boleto_tile/boleto_tile_widget.dart';
+
+class BoletoListWidget extends StatefulWidget {
+  const BoletoListWidget({super.key});
+
+  @override
+  State<BoletoListWidget> createState() => _BoletoListWidgetState();
+}
+
+class _BoletoListWidgetState extends State<BoletoListWidget> {
+  final controller = BoletoListController();
+  @override
+  Widget build(BuildContext context) {
+    return ValueListenableBuilder<List<BoletoModel>>(
+        valueListenable: controller.boletosNotifier,
+        builder: (_, boletos, __) => Column(
+              children: boletos.map((e) => BoletoTileWidget(data: e)).toList(),
+            ));
+  }
+}
