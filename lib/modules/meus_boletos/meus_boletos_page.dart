69d029a9c8946334a6a9eb054c60a7509f87cb07
mathmuniz <mmuniz605@gmail.com>
2022-06-08 15:10:01 -0300

    update

--- lib/modules/meus_boletos/meus_boletos_page.dart
+++ lib/modules/meus_boletos/meus_boletos_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
+import 'package:payflow/shared/widgets/boleto_info/boleto_info_widget.dart';
+import 'package:payflow/shared/widgets/boleto_list/boleto_list_widget.dart';
 
 class MeusBoletosPage extends StatefulWidget {
   const MeusBoletosPage({super.key});
@@ -12,19 +14,33 @@ class MeusBoletosPage extends StatefulWidget {
 class _MeusBoletosPageState extends State<MeusBoletosPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Text(
-          'Meus Boletos',
-          style: TextStyles.titleBoldHeading,
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(vertical: 24),
-          child: Divider(
-            color: AppColors.stroke,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 24),
+      child: Column(
+        children: [
+          BoletoInfoWidget(size: 14),
+          Padding(
+            padding: const EdgeInsets.only(top: 24),
+            child: Row(
+              children: [
+                Text(
+                  'Meus Boletos',
+                  style: TextStyles.titleBoldHeading,
+                ),
+              ],
+            ),
           ),
-        ),
-      ],
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 24),
+            child: Divider(
+              thickness: 1,
+              height: 1,
+              color: AppColors.stroke,
+            ),
+          ),
+          BoletoListWidget()
+        ],
+      ),
     );
   }
 }
