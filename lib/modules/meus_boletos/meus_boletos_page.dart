b40f76bb87a768c4d4f09bad2ddc291f7aa60618
mathmuniz <mmuniz605@gmail.com>
2022-06-09 15:29:16 -0300

    update

--- lib/modules/meus_boletos/meus_boletos_page.dart
+++ lib/modules/meus_boletos/meus_boletos_page.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, sized_box_for_whitespace
+
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_text_styles.dart';
@@ -14,33 +16,45 @@ class MeusBoletosPage extends StatefulWidget {
 class _MeusBoletosPageState extends State<MeusBoletosPage> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
-        children: [
-          BoletoInfoWidget(size: 14),
-          Padding(
-            padding: const EdgeInsets.only(top: 24),
-            child: Row(
-              children: [
-                Text(
-                  'Meus Boletos',
-                  style: TextStyles.titleBoldHeading,
-                ),
-              ],
-            ),
+    return Column(
+      children: [
+        Container(
+          height: 140,
+          child: Stack(
+            children: [
+              Container(
+                color: AppColors.primary,
+                height: 40,
+                width: double.maxFinite,
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 24),
+                child: BoletoInfoWidget(size: 14),
+              ),
+            ],
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(vertical: 24),
-            child: Divider(
-              thickness: 1,
-              height: 1,
-              color: AppColors.stroke,
-            ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 24),
+          child: Row(
+            children: [
+              Text(
+                'Meus Boletos',
+                style: TextStyles.titleBoldHeading,
+              ),
+            ],
           ),
-          BoletoListWidget()
-        ],
-      ),
+        ),
+        Padding(
+          padding: const EdgeInsets.symmetric(vertical: 24),
+          child: Divider(
+            thickness: 1,
+            height: 1,
+            color: AppColors.stroke,
+          ),
+        ),
+        BoletoListWidget()
+      ],
     );
   }
 }
