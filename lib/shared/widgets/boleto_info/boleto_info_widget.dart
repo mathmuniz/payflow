7e597c4327e88dd0acef5d844797007fee117df9
mathmuniz <mmuniz605@gmail.com>
2022-06-09 15:27:10 -0300

    update

--- lib/shared/widgets/boleto_info/boleto_info_widget.dart
+++ lib/shared/widgets/boleto_info/boleto_info_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payflow/shared/themes/app_colors.dart';
 import 'package:payflow/shared/themes/app_images.dart';
+import 'package:payflow/shared/themes/app_text_styles.dart';
 
 class BoletoInfoWidget extends StatelessWidget {
   final int size;
@@ -13,12 +14,40 @@ class BoletoInfoWidget extends StatelessWidget {
         color: AppColors.secondary,
         borderRadius: BorderRadius.circular(5),
       ),
-      child: Row(
-        children: [
-          Image.asset(
-            AppImages.logomini,
-          )
-        ],
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 24),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Image.asset(
+              AppImages.logomini,
+              color: AppColors.background,
+              width: 56,
+              height: 34,
+            ),
+            Container(
+              width: 1,
+              height: 32,
+              color: AppColors.background,
+            ),
+            Text.rich(
+              TextSpan(
+                text: 'Você tem',
+                style: TextStyles.captionBackground,
+                children: [
+                  TextSpan(
+                    text: '$size boletos\n',
+                    style: TextStyles.captionBoldBackground,
+                  ),
+                  TextSpan(
+                    text: 'cadastrados para pagar',
+                    style: TextStyles.captionBackground,
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
